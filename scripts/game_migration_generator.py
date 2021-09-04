be4d00e277548e8d4ced58993eef7636e3b7a23c
Senyao Hou <senyao.hou@gmail.com>
2021-09-04 20:25:13 +0200

    Generate for languages if locale not given

--- scripts/game_migration_generator.py
+++ scripts/game_migration_generator.py
@@ -6,16 +6,15 @@ from constants import LOCALES, COUNTRIES, LOCALES_FULL, TEMPLATE_TYPE
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--locale", type=str,
-                    help="Locale for which to generate migration files",
-                    required=True)
 parser.add_argument("--outputfolder", type=str,
                     help="Path to migration folder",
                     required=True)
+parser.add_argument("--locale", type=str,
+                    help="Locale for which to generate migration files",
+                    )
 parser.add_argument("--template", type=str,
                     help="random-questions, would-you-rather-questions or words. Generate for all if not given."
                     )
-
 args = parser.parse_args()
 
 def generate_migration(locale, template_type, output_folder):
@@ -31,8 +30,16 @@ def generate_migration(locale, template_type, output_folder):
 
 
 if __name__ == "__main__":
-    if args.template:
-        generate_migration(args.locale, args.template, args.outputfolder)
+    if args.locale:
+        if args.template:
+            generate_migration(args.locale, args.template, args.outputfolder)
+        else:
+            for template in TEMPLATE_TYPE:
+                generate_migration(args.locale, template, args.outputfolder)
     else:
-        for template in TEMPLATE_TYPE:
-            generate_migration(args.locale, template, args.outputfolder)
+        for locale in LOCALES:
+            if args.template:
+                generate_migration(locale, args.template, args.outputfolder)
+            else:
+                for template in TEMPLATE_TYPE:
+                    generate_migration(locale, template, args.outputfolder)
